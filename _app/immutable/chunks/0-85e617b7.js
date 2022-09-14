343f3e8cdf0db1751fde315af910a2a825260611
Bernhard Mühl <bernhard.muehl@vbcf.ac.at>
2022-09-14 14:32:37 +0200

    Updates

---
+++ _app/immutable/chunks/0-85e617b7.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-dfa4b11f.js";import{default as t}from"../components/pages/_layout.svelte-fddef01e.js";export{t as component,r as shared};
--- _app/immutable/chunks/1-5bf8e22a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-bf9f8784.js";export{t as component};
